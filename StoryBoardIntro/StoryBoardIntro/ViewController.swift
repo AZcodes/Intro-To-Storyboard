aa02e49d1af53c331babe56b594242c8c870e887
AZcodes <51551780+AZcodes@users.noreply.github.com>
2020-08-05 11:33:31 -0400

    my 1st Xcode proj

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Ayesha Zaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a test print statement! Test-test!")
+    }
+
+}
+
